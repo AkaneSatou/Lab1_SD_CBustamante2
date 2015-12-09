140ab80e9073fddd6535bec1f78a99b2284e5969
Claudia Bustamante Arce <hime_tsukino@hotmail.com>
2015-12-08 21:33:02 -0300

    5° version

--- src/tcpserver/TCPServer.java
+++ src/tcpserver/TCPServer.java
@@ -9,10 +9,15 @@ import java.io.*;
 import java.net.*;
 import java.text.Format;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -22,6 +27,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
 
 
 /**
@@ -64,7 +70,11 @@ public static int id;
                                         
                     if(consultar.equals(prueba)){
                         System.out.println("lo encontre!");
-                       // aumentarVisita();
+                        LRU(consulta);
+                        String prueba2 = eElement.getElementsByTagName("cantidadvecesconsultado").item(0).getTextContent();
+                        int prueba3 = Integer.parseInt(prueba2)+1;
+                        eElement.getElementsByTagName("cantidadvecesconsultado").item(0).setTextContent(Integer.toString(prueba3));
+////                    System.out.println("consultado: " +eElement.getElementsByTagName("cantidadvecesconsultado").item(0).getTextContent());
                         NodeList nlist2 = eElement.getElementsByTagName("respuesta");
                         //return resultado;
                         ArrayList<String> listaResultado = new ArrayList<>();
@@ -74,6 +84,11 @@ public static int id;
                             System.out.println("Resultado " + temp + ": "+eElement2.getElementsByTagName("stringrespuesta").item(0).getTextContent());
                             listaResultado.add(eElement2.getElementsByTagName("stringrespuesta").item(0).getTextContent());
                         }
+                        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                        Transformer transformer = transformerFactory.newTransformer();
+                        DOMSource source = new DOMSource(doc);
+                        StreamResult result = new StreamResult(new File("C:\\Users\\Clau\\Documents\\GitHub\\Lab1_SD_CBustamante\\cache\\" +archivo));
+                        transformer.transform(source, result);
                         busqueda = listaResultado;
                         //return busqueda;   
                     }else{
@@ -82,10 +97,80 @@ public static int id;
                     }
                 }
             }
+            
         }catch (Exception e){
             e.printStackTrace();
         }
     }	
+    
+    public static void LRU(String consulta)
+    {
+    try {
+        File fXmlFile = new File("C:\\Users\\Clau\\Documents\\GitHub\\Lab1_SD_CBustamante\\consultasrealizadas.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
+        
+        doc.getDocumentElement().normalize();
+        
+        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+        NodeList nList = doc.getElementsByTagName("consulta");
+        int variable = 0;
+        for (int temp = 0; temp < nList.getLength(); temp++){
+            Node nNode = nList.item(temp);
+            System.out.println("\nCurrent Element :" + nNode.getNodeName());
+            if (nNode.getNodeType() == Node.ELEMENT_NODE){
+                Element eElement = (Element) nNode;
+                System.out.println("String en cache : " + eElement.getElementsByTagName("string").item(0).getTextContent());
+                String pruebaConsulta = eElement.getElementsByTagName("string").item(0).getTextContent();
+                if(pruebaConsulta.equals(consulta))
+                {
+                    String pruebaConsulta2 = eElement.getElementsByTagName("cantidad").item(0).getTextContent();
+                    int prueba2 = Integer.parseInt(pruebaConsulta2) + 1;
+                    eElement.getElementsByTagName("cantidad").item(0).setTextContent(Integer.toString(prueba2));
+                    variable = 1;
+                }
+            }
+        }
+        if (variable == 0)
+        {
+            Element root = doc.getDocumentElement();
+            Element newElement = doc.createElement("consulta");
+            Element string = doc.createElement("string");
+            string.appendChild(doc.createTextNode(consulta));
+            newElement.appendChild(string);
+            Element cantidad = doc.createElement("cantidad");
+            cantidad.appendChild(doc.createTextNode("1"));
+            newElement.appendChild(cantidad);
+            root.appendChild(newElement);
+            doc.getDocumentElement().normalize();
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("C:\\Users\\Clau\\Documents\\GitHub\\Lab1_SD_CBustamante\\consultasrealizadas.xml"));
+            transformer.transform(source, result);
+        }
+        else
+        {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("C:\\Users\\Clau\\Documents\\GitHub\\Lab1_SD_CBustamante\\consultasrealizadas.xml"));
+            transformer.transform(source, result);
+        }
+        
+    } catch (ParserConfigurationException ex) {
+        Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (SAXException ex) {
+        Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (IOException ex) {
+        Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (TransformerConfigurationException ex) {
+        Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (TransformerException ex) {
+        Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }
     
     public static int leerXML(String parts){ //lee el xml de preposiciones o palabras basura
         try {
@@ -169,8 +254,7 @@ public static int id;
             //Buffer para recibir desde el cliente
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             //Buffer para enviar al cliente
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            
+                        
             //Recibimos el dato del cliente y lo mostramos en el server
             fromClient =inFromClient.readLine();
             System.out.println("Received: " + fromClient);
@@ -178,7 +262,6 @@ public static int id;
             //Se procesa el dato recibido
             processedData = fromClient.toLowerCase()+ '\n' ; //toma el mensaje del cliente y lo pasa a minusculas
             String resultado = procesarQuery(processedData); //crea la lista de palabras de la consulta y elimina preposiciones
-            
             String ConsultaLista = new StringBuffer(resultado).toString() + '\n';
             System.out.println("esta es la consulta final: " +ConsultaLista); //lista para buscar el resultado en caché
             //consulta = ConsultaLista; //actualizo variable global
@@ -195,16 +278,12 @@ public static int id;
             }
             
             for (int j = 0; j < threads.length; j++) {
-                id = j;
+                //id = j;
                 //System.out.println(j);
                 final int i = j;
                 threads[j] = new Thread(() -> {
-                    //System.out.println("leer archivo:" +ficheros[id].getName());
-                    //aca cada hebra debe buscar en un archivo
                     System.out.println("int: " + i);
                     compareCacheConsulta(ficheros[i].getName(), consulta);
-                    
-                    //System.out.println(ficheros[0]);
                 });
                 threads[j].start();
             }
@@ -223,7 +302,6 @@ public static int id;
             busqueda = null;
             ObjectOutputStream objectOutput = new ObjectOutputStream(connectionSocket.getOutputStream());
             objectOutput.writeObject(resultado2);
-            //outToClient.writeBytes(ConsultaLista);
 
         }
     }
